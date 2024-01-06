dab3e7996ee6093d87013832b82643d81ae5a9ac
Goodness Okoro <ceciliaokoro2@gmail.com>
2024-01-06 04:05:30 +0100

    new commit

---
+++ JS_Exercises/Day5/web_techs.js
@@ -0,0 +1,18 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+let search = "Sass"
+webTechs.includes(search)
+? console.log(`${search} is a CSS preprocess`)
+: countries.push(search) && console.log(countries)
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+let fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)
