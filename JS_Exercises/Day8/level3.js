34024224f30bfb58542a44c42189227889d04aa2
Goodness Okoro <ceciliaokoro2@gmail.com>
2024-01-14 15:59:08 +0100

    new commit

--- JS_Exercises/Day8/level3.js
+++ JS_Exercises/Day8/level3.js
@@ -120,20 +120,28 @@
 // inform the user that he has already an account.
 function signUp (users){
   let  id = prompt("create an id: "), username = prompt("Username: "),email = prompt("Email: "), password = prompt("Password: "),statement =""
+  const newUser = {}, now = new Date()
     for(person in users){
         if((users[person]._id.includes(id)) && users[person]._id.includes(username)){
            statement = `You already have an account`
         }
         else{
-            users.id = "hello"
-            statement = `You have an account`
+            newUser._id =id,
+            newUser.username = username,
+            newUser.email = email,
+            newUser.password = password,
+            newUser.createdAt = `${now.getDay+1}/${now.getMonth + 1}/ ${now.getFullYear}/${now.getHours}/${now.getMinutes}`
+            isLoggedIn = false
+            }
         }
     }
     return statement
-}
 console.log(signUp(users))
 // b. Create a function called signIn which allows user to sign in to the application
 
 // The products array has three elements and each of them has six properties. a. Create a function called rateProduct which rates the product b. Create a function called averageRating which calculate the average rating of a product
 
 // Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
+
+//         createdAt:'08/01/2020 10:00 AM',
+//         isLoggedIn: false
